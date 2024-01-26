fa882268b0473b729f713468e4443dc7955becd3
Sagiru20 <sagiru203@gmail.com>
2024-01-26 20:25:21 +0200

    Add recycler view for dog posts list

--- app/src/main/java/com/buddy4life/MainActivity.kt
+++ app/src/main/java/com/buddy4life/MainActivity.kt
@@ -3,9 +3,10 @@ package com.buddy4life
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_posts)
     }
 }
