3f916201540e478cfc0d0c4d57f2b084d7838b77
Sagiru20 <sagiru203@gmail.com>
2024-01-31 14:44:12 +0200

    Fix navigation and hide bottom navigation bar on add post page

--- app/src/main/java/com/buddy4life/MainActivity.kt
+++ app/src/main/java/com/buddy4life/MainActivity.kt
@@ -2,52 +2,16 @@ package com.buddy4life
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.MenuProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private var navController: NavController? = null
     private var bottomNavigationView: BottomNavigationView? = null
-
-    private val menuProvider = object : MenuProvider {
-        override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-            menuInflater.inflate(R.menu.bottom_menu, menu)
-        }
-
-        override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-            return when (menuItem.itemId) {
-                R.id.addPostFragment -> {
-                    Log.i("HERE", "add post!!!!!")
-                    bottomNavigationView?.visibility = View.GONE
-                    true
-                }
-
-                android.R.id.home -> {
-                    Log.i("HERE", "home!!!!!")
-                    bottomNavigationView?.visibility = View.GONE
-                    true
-                }
-
-                else -> navController?.let {
-                    Log.i("HERE", "else!!!!!")
-                    bottomNavigationView?.visibility = View.VISIBLE
-                    NavigationUI.onNavDestinationSelected(menuItem, it)
-                } ?: false
-            }
-        }
-
-        override fun onPrepareMenu(menu: Menu) {
-            super.onPrepareMenu(menu)
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -57,8 +21,16 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.navHostMain) as? NavHostFragment
         navController = navHostFragment?.navController
         bottomNavigationView = findViewById(R.id.bottom_navigation)
-        navController?.let { NavigationUI.setupWithNavController(bottomNavigationView!!, it) }
+        bottomNavigationView?.setupWithNavController(navController!!)
 
-        addMenuProvider(menuProvider)
+        navController?.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.addPostFragment) {
+                Log.i("HERE", "add post!!!!!")
+                bottomNavigationView?.visibility = View.GONE
+            } else {
+                Log.i("HERE", "else!!!!!")
+                bottomNavigationView?.visibility = View.VISIBLE
+            }
+        }
     }
 }
