3a3f024aae2ad06a23423e24fd6183250ee9a75e
28ori <282ori@gmail.com>
2024-02-24 15:29:10 +0200

    reformating

--- app/src/main/java/com/buddy4life/dao/UserDao.kt
+++ app/src/main/java/com/buddy4life/dao/UserDao.kt
@@ -4,14 +4,11 @@ import androidx.room.Dao
 import androidx.room.Query
 import com.buddy4life.model.Post.Post
 
+@Dao
 interface UserDao {
-
-    @Dao
-    interface PostDao {
 
         @Query("SELECT * FROM Post WHERE id LIKE :id LIMIT 1")
         fun getById(id: Long): Post
-    }
 
 
 }
